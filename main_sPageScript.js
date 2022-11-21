7c7a568aff82ea4db88cf2b8d68cb8fa17a7f923
inan yigit <inanygt@hotmail.com>
2022-11-21 13:54:53 +0100

    added age

--- main_sPageScript.js
+++ main_sPageScript.js
@@ -4,8 +4,18 @@ let session = JSON.parse(sessionStorage.getItem('loginObject'));
 
 let user = document.getElementById("user");
 let bYear = document.getElementById("bYear");
-// let location = document.getElementById("location");
+let place = document.getElementById("place");
+let age = document.getElementById("age");
 
 user.innerHTML = local.user;
-// location.innerHTML = local.location;
-bYear.innerHTML = local.bdate;
+place.innerHTML = local.location;
+bYear.innerHTML = local.bdate.slice(0, 4);
+
+const currentYear = new Date().getFullYear();
+let cAge = currentYear - local.bdate.slice(0, 4);
+age.innerHTML = cAge;
+
+
+
+
+// console.log(cAge)
